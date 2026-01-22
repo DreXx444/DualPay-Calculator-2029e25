d67174a547c2f804f45b2b5f1e261dd71321eddf
Sergey <davidnorair@gmail.com>
2026-01-22 19:17:28 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,14 @@
-const CACHE_NAME = 'dualpay-2026-v1';
+const CACHE_NAME = 'dp-v1';
 const ASSETS = [
     'index.html',
     'manifest.json',
-    'https://cdn.tailwindcss.com',
-    'https://fonts.googleapis.com/css2?family=Comfortaa:wght@700&family=Montserrat:wght@400;600;900&display=swap'
+    'https://cdn.tailwindcss.com'
 ];
 
-// Инсталиране и кеширане
 self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-    );
+    e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
 });
 
-// Работа офлайн (извличане от кеша)
 self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then((response) => response || fetch(e.request))
-    );
+    e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
 });
