bc7ba2a359fb58b5ce3a348a7f5c7971fd42d745
Sergey <davidnorair@gmail.com>
2026-01-22 20:05:58 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,11 @@
-const CACHE_NAME = 'dualpay-v4';
-const ASSETS = ['./index.html', './manifest.json'];
-
+const CACHE_NAME = 'dualpay-v5';
 self.addEventListener('install', (e) => {
     self.skipWaiting();
-    e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+    e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(['index.html', 'manifest.json'])));
 });
-
 self.addEventListener('activate', (e) => {
     e.waitUntil(caches.keys().then((ks) => Promise.all(ks.map((k) => k !== CACHE_NAME && caches.delete(k)))));
 });
-
 self.addEventListener('fetch', (e) => {
     e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
 });
